0303d8ad9ae319ee01ecc907d5a0507727d10f0f
huanglijun@ppdai.com <huanglijun@ppdai.com>
2019-01-22 14:48:58 +0800

    init commit

---
+++ src/main/java/com/example/platform0122/Platform0122Application.java
@@ -0,0 +1,14 @@
+package com.example.platform0122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Platform0122Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Platform0122Application.class, args);
+    }
+
+}
+
